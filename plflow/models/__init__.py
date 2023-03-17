afc44785d9699fb6d49de0dcaadac0ff046e236c
rexxy-sasori <hgeng4@illinois.edu>
2023-03-17 12:18:37 +0800

    refactor

--- plflow/models/__init__.py
+++ plflow/models/__init__.py
@@ -1,3 +1,3 @@
 from plflow.models.cnns import *
 from plflow.models.transformers import *
-from plflow.models.transformers.swin_transformers import build_swin_transformers
+from plflow.models.transformers.swin_transformers import SwinTransformers
